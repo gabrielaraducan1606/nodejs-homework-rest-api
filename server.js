3ef0f710aeef539747de1fb6c2ccfed8044bdccb
gabrielaraducan1606 <165211880+gabrielaraducan1606@users.noreply.github.com>
2025-02-20 01:01:25 +0200

    commit1

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 const app = require("./app");
 
-app.listen(3000, () => {
-    console.log("Server is running. Use our API on port: 3000");
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose.connect(MONGO_URI).then(() => {
+    console.log("Database connection successful");
+    app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+}).catch((error) => {
+    console.error("Database connection failed:", error);
+    process.exit(1);
 });
