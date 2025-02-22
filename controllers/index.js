84df96997726ede242aa9f4bc422d014e240cd5c
gabrielaraducan1606 <165211880+gabrielaraducan1606@users.noreply.github.com>
2025-02-22 17:53:54 +0200

    commit1

--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,6 @@
+const path = require("path");
+const  Jimp  = require("jimp"); 
+
 const {
     getAllUsers,
     getAllContacts,
@@ -106,6 +109,39 @@ const getCurrentUserController = async (req, res) => {
     }
 };
 
+const updateAvatarController = async (req, res, next) => {
+    try {
+        console.log("File:", req.file);
+
+        if (!req.file || !req.file.path) {
+            return res.status(400).json({ message: "No file uploaded or invalid file path." });
+        }
+
+        if (!req.user) {
+            return res.status(401).json({ message: "User not authenticated." });
+        }
+
+        if (!req.file.originalname) {
+            return res.status(400).json({ message: "Invalid file name." });
+        }
+
+        const uniqFilename = `${req.user._id}-${Date.now()}${path.extname(req.file.originalname)}`;
+        const destinationPath = path.join(__dirname, `../public/avatars/${uniqFilename}`);
+
+        const image = await Jimp.read(req.file.path); 
+        image.resize(256, 256); 
+        await image.writeAsync(destinationPath); 
+
+        req.user.avatarURL = `/avatars/${uniqFilename}`;
+        await req.user.save();
+
+        res.status(200).json({ avatarURL: req.user.avatarURL });
+    } catch (error) {
+        console.error("Eroare detaliată:", error);
+        res.status(500).json({ message: error.message });
+    }
+};
+
 module.exports = {
     getUsersController,
     getContactsController,
@@ -114,4 +150,5 @@ module.exports = {
     logoutUserController,
     addContactController,
     getCurrentUserController,
+    updateAvatarController
 };
